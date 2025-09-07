c94d4c4a6bf7ff8932a9e7e31007d6493b5a47a0
Roberto Albergo <ralbergo7@gmail.com>
2025-09-07 12:55:56 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+import NavBar from './components/NavBar'
+import ClassComponent from "./components/ClassComponent"
+
+function App() {
+  
+  return (
+    <>
+     <NavBar />
+     <ClassComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ClassComponent.tsx
@@ -0,0 +1,15 @@
+
+import { Component } from 'react'
+
+class ClassComponent extends Component {
+  render() {
+   return( 
+    <div>
+       <h2>ClassComponent</h2>
+    </div>
+   )
+}
+   }
+   
+export default ClassComponent
+        
