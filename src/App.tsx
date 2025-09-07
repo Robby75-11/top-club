35e8557c68bbc15859aca2008ab0af3aae8d699b
Roberto Albergo <ralbergo7@gmail.com>
2025-09-07 18:02:17 +0200

    button

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 import NavBar from './components/NavBar'
 import ClassComponent from "./components/ClassComponent"
+import {Row, Col, Container} from 'react-bootstrap'
 
 function App() {
   
   return (
     <>
+
      <NavBar />
-     <ClassComponent />
+     <Container>
+ <Row className="justify-content-md-center mt-5">
+  <Col xs={12} md={6} className="text-center">
+     <ClassComponent sottotitolo="Contenuto del sottotitolo"
+     value={100} />
+    </Col>
+     </Row>
+     </Container>
     </>
-  )
+  ) 
 }
 
 export default App
--- src/components/ClassComponent.tsx
+++ src/components/ClassComponent.tsx
@@ -1,11 +1,35 @@
-
 import { Component } from 'react'
+import { Button } from 'react-bootstrap'
 
-class ClassComponent extends Component {
+interface ClassComponentProps {
+    sottotitolo: string
+    value?: number
+}
+interface ClassComponentState {
+    name: string
+}
+
+class ClassComponent extends Component<
+ClassComponentProps,
+ClassComponentState
+> {
+    state= {
+        name: "Mario",
+    }
   render() {
    return( 
     <div>
        <h2>ClassComponent TS</h2>
+       <h4>{this.props.sottotitolo}</h4>
+       <p>Il Contatore vale: {this.props.value || 0}</p>
+       <p>il tuo nome è {this.state.name.toUpperCase()}</p>
+       <Button variant="success"onClick={() =>
+        this.setState({
+            name: this.state.name === "Luigi" ? "Mario" : "Luigi",
+        })
+       }>
+        Cambia Nome
+       </Button>
     </div>
    )
 }
